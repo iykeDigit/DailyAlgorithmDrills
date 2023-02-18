62e15d218924a9fa5e72b25616fc2b026bbfa8c4
unknown <njokuiyke01@gmail.com>
2023-02-18 11:27:39 -0500

    Completed the neetcode Two pointers category

--- TwoPointers/TwoPointers/Program.cs
+++ TwoPointers/TwoPointers/Program.cs
@@ -8,10 +8,149 @@ namespace TwoPointers
         static void Main(string[] args)
         {
             //var result = IsPalindrome("A man, a plan, a canal: Panama");
-            var arr = new int[] { -1, 0, 1, 2, -1, -4 };
-            Console.WriteLine(arr[arr.Length-1]);
-            var result = ThreeSum(arr);
+            var arr = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            
+            var result = OptimalTrap(arr);
             Console.WriteLine("");
+        }
+
+        public static int OptimalTrap(int[] height) 
+        {
+            var left = 0;
+            var right = height.Length - 1;
+            var leftMax = height[left];
+            var rightMax = height[right];
+            var res = 0;
+            while(left < right) 
+            {
+                if(leftMax < rightMax) 
+                {
+                    left++;
+                    leftMax = Math.Max(leftMax, height[left]);
+                    res += leftMax - height[left];
+                }
+                else 
+                {
+                    right--;
+                    rightMax = Math.Max(rightMax, height[right]);
+                    res += rightMax - height[rightMax];
+                }
+            }
+            return res;
+        }
+
+        public static int Trap(int[] height)
+        {
+            var maxRight = new int[height.Length];
+            var maxLeft = new int[height.Length];
+            int result = 0;
+            var max = int.MinValue;
+            for(int i = 1; i < height.Length; i++) 
+            {
+                max = Math.Max(max, height[i - 1]);
+                maxRight[i] = max;
+            }
+
+            max = int.MinValue;
+
+            for(int i = height.Length-2; i >=0; i--) 
+            {
+                max = Math.Max(max, height[i + 1]);
+                maxLeft[i] = max;
+            }
+
+            for(int i = 0; i < height.Length; i++) 
+            {
+                int val = Math.Min(maxRight[i], maxLeft[i]) - height[i];
+                if (val < 0) continue;
+                result += val;
+            }
+            
+            
+            return result;
+        }
+
+        public static int MaxArea(int[] height) 
+        {
+            int first = 0;
+            int second = height.Length - 1;
+            int max = 0;
+
+            while(first < second) 
+            {
+                if(height[first] < height[second]) 
+                {
+                    max = Math.Max(max, height[first] * (second - first));
+                    first++;
+                }
+                else 
+                {
+                    max = Math.Max(max, height[second] * (second - first));
+                    second--;
+                }
+            }
+            return max;
+        }
+
+        public static int BruteMaxArea(int[] height) 
+        {
+            int res = 0;
+            for(int i = 0; i < height.Length; i++) 
+            {
+                for(int r = i+1; r <
+                    height.Length; r++) 
+                {
+                    Console.WriteLine($"i: {i}, height[i]: {height[i]}");
+                    Console.WriteLine($"r: {r}, height[r]: {height[r]}");
+                    int area = (r - i) * Math.Min(height[i], height[r]);
+                    res = Math.Max(res, area);
+                    Console.WriteLine($"res: {res}");
+                    Console.WriteLine();
+                }
+            }
+            return res;
+        }
+
+        public static IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (nums.Length < 4) return result;
+            Array.Sort(nums);
+            long val = (long)(int)target;
+            for (int i = 0; i < nums.Length - 3; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                for (int j = i + 1; j < nums.Length - 2; j++)
+                {
+                    if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                    int low = j + 1;
+                    int high = nums.Length - 1;
+                    long newTarget = val - nums[i] - nums[j];
+                    while (low < high)
+                    {
+                        if (nums[low] + nums[high] == newTarget)
+                        {
+                            result.Add(new List<int> { nums[i], nums[j], nums[low], nums[high] });
+                            while (low < high && nums[low] == nums[low + 1]) low++;
+                            while (low < high && nums[high] == nums[high - 1]) high--;
+                            low++;
+                            high--;
+
+                        }
+                        else if (nums[low] + nums[high] < newTarget)
+                        {
+                            low++;
+                            while (low < high && nums[low] == nums[low - 1]) low++;
+                        }
+                        else
+                        {
+                            high--;
+                            while (low < high && nums[high] == nums[high + 1]) high--;
+                        }
+                    }
+                }
+            }
+            return result;
         }
 
         public static bool IsPalindrome(string s)
@@ -62,24 +201,24 @@ namespace TwoPointers
         {
             Array.Sort(nums);
             var result = new List<IList<int>>();
-            for(int i = 0; i < nums.Length-2; i++) 
+            for (int i = 0; i < nums.Length - 2; i++)
             {
-                if(i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+                if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
                 {
                     int left = i + 1;
-                    int right = nums.Length-1;
+                    int right = nums.Length - 1;
                     int sum = 0 - nums[i];
 
-                    while(left < right) 
+                    while (left < right)
                     {
-                        if(nums[left] + nums[right] == sum) 
+                        if (nums[left] + nums[right] == sum)
                         {
                             result.Add(new List<int> { nums[i], nums[left], nums[right] });
-                            while(left < right && nums[left] == nums[left + 1])
+                            while (left < right && nums[left] == nums[left + 1])
                             {
                                 left++;
                             }
-                            while(left < right && nums[right] == nums[right - 1]) 
+                            while (left < right && nums[right] == nums[right - 1])
                             {
                                 right--;
                             }
@@ -87,7 +226,7 @@ namespace TwoPointers
                             left++;
                             right--;
                         }
-                        else if(nums[left] + nums[right] > sum) 
+                        else if (nums[left] + nums[right] > sum)
                         {
                             right--;
                         }
@@ -100,5 +239,6 @@ namespace TwoPointers
             }
             return result;
         }
+
     }
 }
